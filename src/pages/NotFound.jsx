de0d65f053c2ae6cf27a1386fdf9de31e2bd8d28
TerryLoc <terryloughran@gmail.com>
2026-02-26 10:48:07 +0000

    Add: Import specific styles for 404 Not Found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import { useEffect, useRef } from 'react';
 import SEO from '../components/SEO';
 import { FadeInUp } from '../components/AnimatedSection';
+import './NotFound.css'; // Import styles specific to the 404 page
 
 
 /**
